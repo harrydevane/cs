5392887043ef5b56ac6c8e5e58e300dfd1537908
Harry <me@harrydevane.com>
2019-12-06 09:00:20 +0000

    Add CO539 Assignment 2

--- co518/assignment2/src/LinklessTree.java
+++ co518/assignment2/src/LinklessTree.java
@@ -1,203 +1,160 @@
+public class LinklessTree<E extends Comparable<? super E>> {
 
-/**
- * Trees without explicit links.
- * Notice that various fields/methods have the protected modifier
- * when normally they would/should be private.
- * The reason is that this supports whitebox testing.
- *
- * @author Stefan Kahrs
- * @version 1
- */
-//note the constraint on A is a slight generalisation of A extends Comparable<A>
-//and is generally recommended when one wants a comparison operation
-//it basically allows that the comparison op is implemented at a supertype
-//of A, instead of A itself;
-//for the assessment itself it makes no discernable difference
-public class LinklessTree<A extends Comparable<? super A>>
-{
-    //sizes of subtrees at that node index
-    protected int[] sizes;
-    protected Object[] elems;
-    //for annoying technical reason this has to be an array of objects
+    private static final int INITIAL_SIZE = 15;
 
-    /**
-     * Constructor for objects of class LinklessTree
-     */
-    private static final int STARTSIZE=15;
-    public LinklessTree()
-    {
-        assert STARTSIZE>0;
-        elems = freshElemArray(STARTSIZE);
-        sizes = new int[STARTSIZE];
-        sizes[0]=0;
+    private Object[] elements;
+    private int[] sizes;
+
+    public LinklessTree() {
+        elements = new Object[INITIAL_SIZE];
+        sizes = new int[INITIAL_SIZE];
+        sizes[0] = 0;
     }
 
-    //size of whole tree is the size of the subtree rooted at 0
     public int size() {
         return getSize(0);
     }
 
-    public A getValue(int index) {
-        return (A)elems[index];
+    public E getValue(int index) {
+        return (E) elements[index];
     }
 
-    //auxiliary methods to index the arrays out of bounds too
-    //they may help to reduce case distinctions
-    protected A getKey(int subtree) {
-        if (subtree>=elems.length) return null; // out of bounds
-        return getValue(subtree);
-    }
-
-    protected int getSize(int subtree) {
-        if (subtree>=elems.length) return 0; // out of bounds
+    public int getSize(int subtree) {
+        if (subtree >= elements.length) {
+            return 0;
+        }
         return sizes[subtree];
     }
 
-    //encapsulates the cast on the allocation
-    protected Object[] freshElemArray(int capacity) {
-        return new Object[capacity];
-    }
-
-    //remainder needs to be modified
-
-    //find index position of val in tree, if there, or where it goes, if not there
-    protected int findIndex(A val) {
-        int index = 0;
-        A elem;
-        while (index < elems.length && (elem = (A) elems[index]) != null) {
-            int value = val.compareTo(elem);
+    public int findIndex(E element) {
+        int currentIndex = 0;
+        E currentElement;
+        while (currentIndex < elements.length && (currentElement = (E) elements[currentIndex]) != null) {
+            int value = element.compareTo(currentElement);
             if (value == 0) {
-                return index;
+                return currentIndex;
             }
-            index = (index * 2) + (value < 0 ? 1 : 2);
+            currentIndex = (currentIndex * 2) + (value < 0 ? 1 : 2);
         }
-        return index;
+        return currentIndex;
     }
 
-    //is value in tree
-    public boolean contains(A val) {
-        int index = 0;
-        A elem;
-        while (index < elems.length && (elem = (A) elems[index]) != null) {
-            int value = val.compareTo(elem);
+    public boolean contains(E element) {
+        int currentIndex = 0;
+        E currentElement;
+        while (currentIndex < elements.length && (currentElement = (E) elements[currentIndex]) != null) {
+            int value = element.compareTo(currentElement);
             if (value == 0) {
                 return true;
             }
-            index = (index * 2) + (value < 0 ? 1 : 2);
+            currentIndex = (currentIndex * 2) + (value < 0 ? 1 : 2);
         }
         return false;
     }
 
-    //grow the space in which we can palce the tree, so that at least one insertion will succeed
-    protected void grow() {
-        Object[] newElems = freshElemArray(elems.length * 2);
+    private void grow() {
+        Object[] newElements = new Object[elements.length * 2];
         int[] newSizes = new int[sizes.length * 2];
-        System.arraycopy(elems, 0, newElems, 0, elems.length);
+        System.arraycopy(elements, 0, newElements, 0, elements.length);
         System.arraycopy(sizes, 0, newSizes, 0, sizes.length);
-        elems = newElems;
+        elements = newElements;
         sizes = newSizes;
     }
 
-    //fetch the i-th element, in comparsion order
-    public A get(int i){
-        if (i < 0 || i >= size()) {
+    public E get(int index) {
+        if (index < 0 || index >= size()) {
             throw new IndexOutOfBoundsException();
         }
-        int index = 0;
-        while (index < elems.length) {
-            int leftSize = getSize((index * 2) + 1);
-            if (i == leftSize) {
-                return (A) elems[index];
-            } else if (i > leftSize) {
-                i -= (leftSize + 1);
-                index = (index * 2) + 2;
+        int currentIndex = 0;
+        while (currentIndex < elements.length) {
+            int leftSize = getSize((currentIndex * 2) + 1);
+            if (index == leftSize) {
+                return (E) elements[currentIndex];
+            } else if (index > leftSize) {
+                index -= (leftSize + 1);
+                currentIndex = (currentIndex * 2) + 2;
             } else {
-                index = (index * 2) + 1;
+                currentIndex = (currentIndex * 2) + 1;
             }
         }
         return null;
     }
 
-    //add x to tree, return true if tree was modified
-    //we do not allow multiple copies of the equal objects in tree
-    //equality is decided by using compareTo
-    public boolean insert(A x){
-        int index = 0;
-        A elem;
-        while (index < elems.length && (elem = (A) elems[index]) != null) {
-            int value = x.compareTo(elem);
+    public boolean insert(E element) {
+        int currentIndex = 0;
+        E currentElement;
+        while (currentIndex < elements.length && (currentElement = (E) elements[currentIndex]) != null) {
+            int value = element.compareTo(currentElement);
             if (value == 0) {
                 return false; // duplicate
             }
-            index = (index * 2) + (value < 0 ? 1 : 2);
+            currentIndex = (currentIndex * 2) + (value < 0 ? 1 : 2);
         }
-        while (index >= elems.length) {
+        while (currentIndex >= elements.length) {
             grow();
         }
-        elems[index] = x;
-        modifySizes(0, index, x, 1);
+        elements[currentIndex] = element;
+        modifySizes(0, currentIndex, element, 1);
         return true;
     }
 
-    //remove x from tree, return true if tree was modified
-    public boolean delete(A x){
-        int index = 0;
-        A elem;
-        while (index < elems.length && (elem = (A) elems[index]) != null) {
-            int value = x.compareTo(elem);
+    public boolean delete(E element) {
+        int currentIndex = 0;
+        E currentElement;
+        while (currentIndex < elements.length && (currentElement = (E) elements[currentIndex]) != null) {
+            int value = element.compareTo(currentElement);
             if (value == 0) {
-                int leftSize = sizes[(index * 2) + 1];
-                int rightSize = sizes[(index * 2) + 2];
-                modifySizes(0, index, x, -1);
+                int leftSize = sizes[(currentIndex * 2) + 1];
+                int rightSize = sizes[(currentIndex * 2) + 2];
+                modifySizes(0, currentIndex, element, -1);
                 if (leftSize > 0 && leftSize >= rightSize) {
-                    elems[index] = deleteLargest((index * 2) + 1);
+                    elements[currentIndex] = deleteLargest((currentIndex * 2) + 1);
                 } else if (rightSize > leftSize) {
-                    elems[index] = deleteSmallest((index * 2) + 2);
+                    elements[currentIndex] = deleteSmallest((currentIndex * 2) + 2);
                 } else {
-                    elems[index] = null;
+                    elements[currentIndex] = null;
                 }
                 return true;
             }
-            index = (index * 2) + (value < 0 ? 1 : 2);
+            currentIndex = (currentIndex * 2) + (value < 0 ? 1 : 2);
         }
         return false;
     }
 
-    //not requested, but these might be useful auxiliary ops for delete
-    private A deleteLargest(int subtree) {
-        A elem = (A) elems[subtree];
+    private E deleteLargest(int subtree) {
+        E element = (E) elements[subtree];
         sizes[subtree] = sizes[subtree] - 1;
-        if (elems.length > (subtree * 2) + 2) {
-            if (elems[(subtree * 2) + 2] != null) {
+        if (elements.length > (subtree * 2) + 2) {
+            if (elements[(subtree * 2) + 2] != null) {
                 return deleteLargest((subtree * 2) + 2);
             }
         }
-        elems[subtree] = null;
-        return elem;
+        elements[subtree] = null;
+        return element;
     }
 
-    private A deleteSmallest(int subtree) {
-        A elem = (A) elems[subtree];
+    private E deleteSmallest(int subtree) {
+        E element = (E) elements[subtree];
         sizes[subtree] = sizes[subtree] - 1;
-        if (elems.length > (subtree * 2) + 1) {
-            if (elems[(subtree * 2) + 1] != null) {
+        if (elements.length > (subtree * 2) + 1) {
+            if (elements[(subtree * 2) + 1] != null) {
                 return deleteSmallest((subtree * 2) + 1);
             }
         }
-        elems[subtree] = null;
-        return elem;
+        elements[subtree] = null;
+        return element;
     }
 
-    private void modifySizes(int startIndex, int endIndex, A x, int diff) {
-        int index = startIndex;
-        A elem;
-        while (index <= endIndex && (elem = (A) elems[index]) != null) {
-            sizes[index] = sizes[index] + diff;
-            int value = x.compareTo(elem);
+    private void modifySizes(int startIndex, int endIndex, E element, int diff) {
+        int currentIndex = startIndex;
+        E currentElement;
+        while (currentIndex <= endIndex && (currentElement = (E) elements[currentIndex]) != null) {
+            sizes[currentIndex] = sizes[currentIndex] + diff;
+            int value = element.compareTo(currentElement);
             if (value == 0) {
                 break;
             }
-            index = (index * 2) + (value < 0 ? 1 : 2);
+            currentIndex = (currentIndex * 2) + (value < 0 ? 1 : 2);
         }
     }
 }
